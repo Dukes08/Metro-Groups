f38f7c4ec9b275ce365e666c018ad7a41f5415d0
Hugo Duque <hugo.duque@correo.unimet.edu.ve>
2024-03-27 12:01:33 -0400

    Feat: Login primitive functions

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,44 @@
-/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { loginWithEmailAndPassword, signInWithGoogle } from "../../firebase/auth";
 import React from "react";
 
 function LoginPage() {
-  // Contenido del componente de inicio de sesión
-  return <div> Contenido del formulario de inicio de sesión, etc.</div>;
+  const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const onSuccess = () => {
+        navigate("/landing");
+    };
+
+    const onFail = (_error) => {
+        console.log("LOGIN FAILED, Try Again");
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await loginWithEmailAndPassword({ userData: formData, onSuccess, onFail });
+    };
+
+    const onChange = (event) => {
+        setFormData((oldData) => ({
+            ...oldData,
+            [event.target.name]: event.target.value,
+        }));
+    };
+
+    const handleGoogleClick = async () => {
+        await signInWithGoogle({
+            onSuccess: () => navigate("/landing"),
+        });
+    };
+
+    return(
+      <div></div>
+    )
 }
 
 export default LoginPage;
